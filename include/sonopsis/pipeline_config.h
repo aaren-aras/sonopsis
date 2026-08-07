f675996706cfa0383ed9fd3e81c91b62259f49ba
Aaren Arasaratnam <aaren.aras@gmail.com>
2026-08-07 18:17:44 -0400

    Change constants to macros (compatible across C, C++, and CUDA files)

--- include/sonopsis/pipeline_config.h
+++ include/sonopsis/pipeline_config.h
@@ -1,22 +1,24 @@
 // pipeline_config.h
-
 #pragma once // include file once per compilation
 
-struct ImageDims {
-	int height, width;
-};
+//struct ImageDims {
+//	int height, width;
+//};
 
-namespace Constants {
-	/* 1 unique instance of each across entire program */
-	inline constexpr int SAMPLE_RATE = 40'000'000; // Hz
-	inline constexpr int NUM_CHANNELS = 8;
-	inline constexpr float SPEED_OF_SOUND = 1540.0f; // m/s, in tissue
-	inline constexpr float IMAGING_DEPTH = 0.05f; // metres
-	inline constexpr ImageDims OutputImageDims{500, 500};
-	inline constexpr int NUM_RF_LINES = 64;
+/* Only one instance of each across entire program */
 
-	inline constexpr float ROUNDTRIP_TIME = (2.0f * IMAGING_DEPTH) / (SPEED_OF_SOUND); // seconds
-	inline constexpr int NUM_SAMPLES_PER_LINE = static_cast<int>(ROUNDTRIP_TIME * SAMPLE_RATE); 
+// Macros work everywhere (C/C++/CUDA)
+#define SAMPLE_RATE 40000000 // 40 MHz
+#define NUM_CHANNELS 8
+#define SPEED_OF_SOUND 1540.0f // m/s, in tissue
+#define IMAGING_DEPTH 0.05f // metres
+//#define ImageDims OutputImageDims{500, 500}
+#define OUTPUT_IMAGE_HEIGHT 500
+#define OUTPUT_IMAGE_WIDTH 500
 
-}
+#define NUM_RF_LINES 64
+
+#define ROUNDTRIP_TIME (2.0f * IMAGING_DEPTH) / (SPEED_OF_SOUND) // seconds
+#define NUM_SAMPLES_PER_LINE ((int)(ROUNDTRIP_TIME * SAMPLE_RATE)) 
+
 
