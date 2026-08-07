39c796de7a30b8cc6de74f0a70737eb8af20b91d
Aaren Arasaratnam <aaren.aras@gmail.com>
2026-08-07 18:13:23 -0400

    Scrap starter files

--- sonopsis.h
+++
@@ -1,8 +0,0 @@
-﻿// sonopsis.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
